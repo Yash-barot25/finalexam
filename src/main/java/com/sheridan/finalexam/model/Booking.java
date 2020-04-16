30f7168d71380d8387672f8251b0ee99402b406a
yash <yash250619@gmail.com>
2020-04-16 19:30:12 -0400

    New clasess added

---
+++ src/main/java/com/sheridan/finalexam/model/Booking.java
@@ -0,0 +1,31 @@
+package com.sheridan.finalexam.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "booking")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany()
+    @JoinTable(name = "roomtype_booking", joinColumns = @JoinColumn(name = "booking_id"),
+            inverseJoinColumns = @JoinColumn(name="roomtype_id"))
+    private Set<RoomType> roomTypes;
+
+    private int noOfAdult;
+
+    private int noOfSeniorCitizen;
+
+    private int noOfChildren;
+
+
+}
---
+++ src/main/java/com/sheridan/finalexam/model/RoomType.java
@@ -0,0 +1,27 @@
+package com.sheridan.finalexam.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "room_type")
+public class RoomType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    private Double price;
+
+    @ManyToMany(mappedBy = "roomTypes")
+    Set<Booking> bookings;
+
+
+}
