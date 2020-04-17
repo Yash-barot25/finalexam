2dcf48fcb9e3cac20f7e2a1a4a0e2efae1f0e7ab
Samir Patel <sampatel9494@gmail.com>
2020-04-16 23:21:08 -0400

    unit test done

--- src/test/java/com/sheridan/finalexam/FinalExamTests.java
+++ src/test/java/com/sheridan/finalexam/FinalExamTests.java
@@ -1,13 +1,25 @@
 package com.sheridan.finalexam;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sheridan.finalexam.model.User;
+import com.sheridan.finalexam.repository.UserRepository;
 
 @SpringBootTest
 class FinalExamTests {
 
+	@Autowired
+	UserRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	void intialAdminShouldBeFound() {
+		String adminEmail = "admin@sheridan.com";
+		User found = userRepository.findByEmail(adminEmail);
+		assertThat(found.getEmail()).isEqualTo(adminEmail);
 	}
 
 }
