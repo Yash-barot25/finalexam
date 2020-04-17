112e1975a065b68344edbd09288a384aca886bc2
Samir Patel <sampatel9494@gmail.com>
2020-04-16 23:51:54 -0400

    h2 configured

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,11 @@
-INSERT IGNORE INTO roles(id,name) 
-VALUES 	('1','ADMIN'),
-		('2','STAFF'),
-		('3','CUSTOMER');
+INSERT INTO roles(id,name) VALUES ('1','ADMIN');
+INSERT INTO roles(id,name) VALUES ('2','STAFF');
+INSERT INTO roles(id,name) VALUES ('3','CUSTOMER');
 
-INSERT IGNORE INTO users (id, email, first_name, password, role_id)
+INSERT INTO users (id, email, first_name, password, role_id)
 VALUES ('1','admin@sheridan.com', 'Admin', '$2a$10$a3A0JC757FLZqHYVvEJq7uNaYz3AW8320IHORzmljQN43Hkmi4T7C','1');
 
-INSERT IGNORE INTO room_type (id, price, type)
-VALUES 	('1','50.00','Semi Deluxe'),
-		('2','100.00','Deluxe'),
-		('3','150.00','Luxury');
+INSERT INTO room_type (id, price, type) VALUES ('1','50.00','Semi Deluxe');
+INSERT INTO room_type (id, price, type) VALUES ('2','100.00','Deluxe');
+INSERT INTO room_type (id, price, type) VALUES ('3','150.00','Luxury');
 
